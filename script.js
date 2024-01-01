235e59f978d5fefcf088218b61b27a19f9ca9a3b
Shroomy Ralsei <142345364+ShroomyRalsei@users.noreply.github.com>
2024-01-01 16:57:17 -0400

    feat:Add startGame function

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ const ticTacToe = (() => {
 
     let turn = null;
 
+    const startGame = () => {
+        const coinFlip = Math.random();
+
+        function determineFirstTurn(num) {
+            if(num <= 0.5) {
+                console.log("P1 turn");
+                turn = "P1";
+            }
+            else {
+                console.log("P2 turn");
+                turn = "P2";
+            }
+        }
+
+        determineFirstTurn(coinFlip);
+    }
 })();
